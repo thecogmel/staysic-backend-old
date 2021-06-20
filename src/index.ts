cdb36464647d9f7e8721b83cb0fa34a1c2c0509b
Erick Medeiros <erick_mmedeiros@hotmail.com>
2021-06-20 19:51:50 -0300

    initial deploy test configs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
 import * as express from 'express';
+import cors from 'cors';
 
 import './database/connect';
 import routes from './routes';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 // setup express app here
