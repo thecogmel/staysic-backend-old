920d96f56615dc5c96384d1f60efc7642fc13718
Erick Medeiros <erick_mmedeiros@hotmail.com>
2021-06-20 22:58:43 -0300

    .

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,3 +1,19 @@
-import { createConnection } from "typeorm";
+import { createConnection, ConnectionOptions } from 'typeorm';
 
-createConnection().then(() => console.log("Conectado ao banco!"));
+createConnection(<ConnectionOptions>{
+  type: 'postgres',
+
+  // We need add the extra SSL to use heroku on localhost
+  extra: {
+    ssl: true,
+  },
+
+  // Change the next line to use the Heroku postgresql from other environment like localhost, remenber that heroku changes this data periodically for security reasons
+  url:
+    process.env.DATABASE_URL ||
+    'postgres://postgres:memphis2108@localhost:5432/staysic',
+  subscribers: [],
+  synchronize: true,
+})
+  .then(() => console.log('Conectado ao banco!'))
+  .catch(error => console.log(error));
