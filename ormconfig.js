a35096456c8163744953ff2bad72e89f61c498bc
Erick Medeiros <erick_mmedeiros@hotmail.com>
2021-06-20 23:55:02 -0300

    fix ssl tags

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,12 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   synchronize: true,
   logging: false,
   entities: ['build/entity/**/*.js'],
