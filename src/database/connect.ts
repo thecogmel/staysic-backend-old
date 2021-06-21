355646e4914ce8168360f9b12911cc2cd40ab796
Erick Medeiros <erick_mmedeiros@hotmail.com>
2021-06-20 23:49:08 -0300

    connect configs

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -5,15 +5,16 @@ createConnection(<ConnectionOptions>{
 
   // We need add the extra SSL to use heroku on localhost
   extra: {
-    ssl: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
+  ssl: true,
 
   // Change the next line to use the Heroku postgresql from other environment like localhost, remenber that heroku changes this data periodically for security reasons
   url:
     process.env.DATABASE_URL ||
     'postgres://postgres:memphis2108@localhost:5432/staysic',
-  subscribers: [],
-  synchronize: true,
 })
   .then(() => console.log('Conectado ao banco!'))
   .catch(error => console.log(error));
